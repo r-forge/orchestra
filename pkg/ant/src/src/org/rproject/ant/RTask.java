4fc92b50eeee32bef7a62fb87c37bcfee917acd7
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-02 10:04:06 +0000

    added makevars to build ant package, added REngine and JRIEngine to ant-r.jar
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orchestra@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ant/src/src/org/rproject/ant/RTask.java
+++ pkg/ant/src/src/org/rproject/ant/RTask.java
@@ -1,6 +1,7 @@
 package org.rproject.ant;
 
 import org.apache.tools.ant.Task;
+import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.REngineException;
 import org.rosuda.REngine.JRI.JRIEngine;
 
@@ -23,9 +24,14 @@ public abstract class RTask extends Task {
 	private static JRIEngine initR(){
 		JRIEngine r = null ;
 		try {
-			r = new JRIEngine(new String[]{ "--vanilla", "--default-packages=\"methods,rJava,ant\"" }, null ) ;
+			r = new JRIEngine(new String[]{ "--vanilla" }, null ) ;
+			r.parseAndEval( "require( 'ant', quietly= TRUE, character.only = TRUE )" ) ;
 		} catch (REngineException e) {
 			System.out.println("problem creating R engine");
+			e.printStackTrace();
+			System.exit(1) ;
+		} catch (REXPMismatchException e) {
+			System.out.println("problem loading the ant package");
 			e.printStackTrace();
 			System.exit(1) ;
 		} 
