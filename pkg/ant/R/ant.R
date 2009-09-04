4430e720579c7300682d49845724e7989ab8fccd
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-04 08:34:07 +0000

    removed dependency on exec
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orchestra@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ant/R/ant.R
+++ pkg/ant/R/ant.R
@@ -16,6 +16,25 @@ ant <- function( run = TRUE, arguments = commandArgs(TRUE) ){
     rscript( "ant", "ant.R", run = run, arguments = arguments )
 }
 
+rscript <- function( package, script, arguments = commandArgs(TRUE), run = TRUE, dir = "exec" ){
+	
+	Rscript <- file.path( R.home(), "bin", "Rscript" )
+	script  <- system.file( dir, script, package = package )
+	if( !file.exists( script ) ){
+		stop( sprintf( "script '%s' does not exist", script) )
+	}
+	
+	arguments <- if( length( arguments ) == 0 ) "" else paste( arguments, collapse = " " )
+	cmd <- sprintf( '"%s" "%s" %s', Rscript, script, arguments )
+	
+	if( run ){
+		system( cmd )
+	} else {
+		cmd
+	}
+}
+
+
 ant.task.error.handler <- function( e ){
 	self <- get( "self", .GlobalEnv )
 	self$fails()
