71dfa8840d22eb37b553d6b85654232809a34447
romain (R Enthusiast ) <francoisromain@free.fr>
2009-10-18 14:43:34 +0000

    grab necessary classes from rJava (need RJavaClassLoader)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orchestra@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/orchestra/configure.R
+++ pkg/orchestra/configure.R
@@ -1,5 +1,24 @@
 #!/bin/env Rscript
 
+# grab the path where rJava is installed
+rjava.path <- system.file( package = "rJava" )
+if( identical( rjava.path, "" ) ){
+	stop( "could not find the rJava package" )
+}
+build.properties <- c( 
+	readLines( "inst/javasrc/build.properties.in" ), 
+	sprintf( "rjava.path=%s", rjava.path ) )
+cat( build.properties , file = "inst/javasrc/build.properties", sep = "\n" )
+
+# check that JRI is available
+if( !file.exists( file.path( rjava.path, "jri", "JRI.jar" ) )  ){
+	stop( "JRI.jar not available, please reinstall rJava" )
+}
+
+
+# call ant
 setwd( "inst/javasrc" )
 ant::ant()
+setwd( "../.." )
+q( 0, save = "no" )
 
