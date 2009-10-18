20c2dfd8201f866bb00e78c5d6e6b0f7677edc15
romain (R Enthusiast ) <francoisromain@free.fr>
2009-10-18 18:43:22 +0000

    trick jri to load JRIEngine without relying on java.library.path
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orchestra@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/orchestra/inst/javasrc/com/addictedtor/jedit/rengine/jri/JRIEngineService.java
+++ pkg/orchestra/inst/javasrc/com/addictedtor/jedit/rengine/jri/JRIEngineService.java
@@ -1,9 +1,11 @@
 package com.addictedtor.jedit.rengine.jri ;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.gjt.sp.util.Log;
 import org.rosuda.JRI.Rengine;
-import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.REngine;
-import org.rosuda.REngine.REngineException;
 import org.rosuda.REngine.JRI.JRIEngine;
 
 import com.addictedtor.jedit.rengine.REngineService;
@@ -23,15 +25,48 @@ public class JRIEngineService extends REngineService {
 		return jri; 
 	}
 	
+	@SuppressWarnings("unchecked")
 	private static JRIEngine init(){
+		
+		/* so that System.load("jri") failing does not cause System.exit(1), see Rengine */
+		System.setProperty("jri.ignore.ule", "yes" ) ; 		
+		
+		/* use reflection to load the RJavaClassLoader */
+		Class RJCL = null ; 
+		Constructor<?> cons = null ; 
+		ClassLoader rjcl = null ;
+		try{
+			/* the RJavaClassLoader class */
+			RJCL =  Class.forName("RJavaClassLoader" ) ;
+			
+			Class<?>[] clazzes = new Class<?>[]{ String.class, String.class } ;
+			cons = 	RJCL.getConstructor( clazzes ) ; 
+			
+			rjcl = (ClassLoader) cons.newInstance( "/usr/local/lib/R/library/rJava" , "/usr/local/lib/R/library/rJava/libs" ) ;
+			
+			/* use the rjcl to load the jri library without relying on java.library.path */
+			Method findlib = getProtectedMethod( RJCL, "findLibrary" ) ;
+			boolean access = findlib.isAccessible() ;
+			findlib.setAccessible(true ) ;
+			String jrilib = (String)findlib.invoke( rjcl, new Object[]{ "jri" }) ;
+			findlib.setAccessible(access ) ;
+			
+			System.load( jrilib ) ;
+			
+		} catch( Exception e){
+			e.printStackTrace() ;
+		}
+		Log.log( Log.ERROR, null, rjcl.getClass().getName() ) ;
+		Rengine.jriLoaded = true ;
+		
 		JRIEngine engine = null ; 
 		String[] args = { "--save" } ;
 		try{
 			engine = new JRIEngine( args ) ;
 			engine.parseAndEval( "{ require( rJava ); .jinit() }" ) ;
-		} catch( REngineException e){
-			System.out.println( "could not create jri engine" ) ;
-		} catch( REXPMismatchException e){}
+		} catch( Exception e){
+			e.printStackTrace() ;
+		}
 		
 		Rengine rni = null ; 
 		try{
@@ -42,5 +77,19 @@ public class JRIEngineService extends REngineService {
 		}
 		return engine; 
 	}
+	
+	
+	private static Method getProtectedMethod( Class<?> cl, String name){
+		Method[] methodz = cl.getDeclaredMethods() ;
+		Method m = null ;
+		for( int i=0; i<methodz.length; i++){
+			m = methodz[i] ;
+			if( name.equals( m.getName() ) ){
+				return m ; 
+			}
+		}
+		return null ;
+	}
+	
 	
 }
